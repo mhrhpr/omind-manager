dd6a0dc5f62bc6c9367afaebb7ac6a9a3cbe138b
mohammad reza hossseini pour <130241275+mhrhpr@users.noreply.github.com>
2026-09-09 18:43:06 +0330

    style: load OMIND experience polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './omind-platform.css'
 import './omind-console.css'
 import './landing-next.css'
 import './playground.css'
+import './experience-polish.css'
 import type { Metadata } from 'next'
 import PointerFX from './PointerFX'
 
