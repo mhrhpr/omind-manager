b70596f43f68f2a9d50c8b09d9e12f4883daad61
mohammad reza hossseini pour <130241275+mhrhpr@users.noreply.github.com>
2026-09-09 18:33:58 +0330

    feat: load playground visual layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './omind-next.css'
 import './omind-platform.css'
 import './omind-console.css'
 import './landing-next.css'
+import './playground.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
